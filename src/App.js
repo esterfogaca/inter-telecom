dae3c5d81074ca90b8387da3748dcb8f58329af9
esterfogaca <ester@cantarim.com>
2020-03-06 15:55:52 -0300

    fix(CardList): Corrected key generation.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Loading from './components/Loading';
 const App = () => {
   const [ data, setData ] = useState(false);
   const [ categories, setCategories] = useState(false);
+  const [ category, setCategory ] = useState(1);
+  const [ page, setPage ] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,12 +29,20 @@ const App = () => {
   }, []);
 
   const handleCategoryClick = async id => {
+    setData([]);
+    setPage(1);
     const res = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&category_ids=${id}`);
     const json = await res.json();
     setData(json);
+    setCategory(id);
   };
 
-  const handleButtonClick = () => alert('Olá!');
+  const handleButtonClick = async () => {
+    setPage(page + 1);
+    const res = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&page=${page}&category_ids=${category}`);
+    const json = await res.json();
+    setData([...data, ...json]);
+  };
 
   if (data && categories) {
     return (
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,18 +2,23 @@ import React from 'react';
 import './CardList.css';
 
 import Button from './Button';
+import Loading from './Loading';
 
 import Card from './Card';
 
 const CardList = ({ src, handleClick }) => {
-    return (
-        <div className="cardlist">
-            <div className="cards">
-                { src.map( ({ id, url }) => (<Card image={url} key={id} />) ) }
+    if (src.length > 1) {
+        return (
+            <div className="cardlist">
+                <div className="cards">
+                    { src.map( ({ url }, index) => (<Card image={url} key={index} />) ) }
+                </div>
+                <Button handleClick={handleClick}>Load more cats!</Button>
             </div>
-            <Button handleClick={handleClick}>Load more cats!</Button>
-        </div>
-    );
+        );
+    }
+
+    return (<Loading/>);
 };
 
 export default CardList;
